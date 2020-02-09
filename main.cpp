8e054584588e3c61b32d0927e170a68fcf1b2143
rexim <reximkut@gmail.com>
2020-02-10 03:04:59 +0700

    Add player with walking animation

--- main.cpp
+++ main.cpp
@@ -40,8 +40,8 @@ Tile level[LEVEL_HEIGHT][LEVEL_WIDTH] = {
     {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
     {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
     {Tile::Empty, Tile::Wall,  Tile::Empty, Tile::Empty, Tile::Empty},
-    {Tile::Wall,  Tile::Wall,  Tile::Wall,  Tile::Empty, Tile::Empty},
-    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty}
+    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
+    {Tile::Wall,  Tile::Wall,  Tile::Wall,  Tile::Wall,  Tile::Wall},
 };
 
 struct Tile_Texture
@@ -52,9 +52,8 @@ struct Tile_Texture
 
 void render_tile_texture(SDL_Renderer *renderer,
                          Tile_Texture texture,
-                         int x, int y)
+                         SDL_Rect destrect)
 {
-    SDL_Rect destrect = {x, y, TILE_SIZE, TILE_SIZE};
     sdl(SDL_RenderCopy(
             renderer,
             texture.texture,
@@ -72,11 +71,51 @@ void render_level(SDL_Renderer *renderer, Tile_Texture wall_texture)
 
             case Tile::Wall: {
                 sdl(SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255));
-                render_tile_texture(renderer, wall_texture, x * TILE_SIZE, y * TILE_SIZE);
+                render_tile_texture(renderer, wall_texture,
+                                    {x * TILE_SIZE, y * TILE_SIZE,
+                                     TILE_SIZE, TILE_SIZE});
             } break;
             }
         }
     }
+}
+
+SDL_Texture *load_texture_from_png_file(SDL_Renderer *renderer, const char *image_filename)
+{
+    png_image image;
+    memset(&image, 0, sizeof(image));
+    image.version = PNG_IMAGE_VERSION;
+    if (!png_image_begin_read_from_file(&image, image_filename)) {
+        fprintf(stderr, "Could not read file `%s`: %s\n",
+                image_filename, image.message);
+        abort();
+    }
+    image.format = PNG_FORMAT_RGBA;
+    printf("Width: %d, Height: %d\n", image.width, image.height);
+    uint32_t *image_pixels = (uint32_t *) std::malloc(sizeof(uint32_t) * image.width * image.height);
+
+    if (!png_image_finish_read(&image, nullptr, image_pixels, 0, nullptr)) {
+        fprintf(stderr, "libpng pooped itself: %s\n", image.message);
+        abort();
+    }
+
+    SDL_Surface* image_surface =
+        sdl(SDL_CreateRGBSurfaceFrom(image_pixels,
+                                     image.width,
+                                     image.height,
+                                     32,
+                                     image.width * 4,
+                                     0x000000FF,
+                                     0x0000FF00,
+                                     0x00FF0000,
+                                     0xFF000000));
+
+    SDL_Texture *image_texture =
+        sdl(SDL_CreateTextureFromSurface(renderer,
+                                         image_surface));
+    SDL_FreeSurface(image_surface);
+
+    return image_texture;
 }
 
 int main(int argc, char *argv[])
@@ -95,45 +134,44 @@ int main(int argc, char *argv[])
                 SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED));
 
     // TODO: replace fantasy_tiles.png with our own assets
-    const char *tileset_filename = "fantasy_tiles.png";
-    png_image tileset;
-    memset(&tileset, 0, sizeof(tileset));
-    tileset.version = PNG_IMAGE_VERSION;
-    if (!png_image_begin_read_from_file(&tileset, tileset_filename)) {
-        fprintf(stderr, "Could not read file `%s`: %s\n",
-                tileset_filename, tileset.message);
-        abort();
-    }
-    tileset.format = PNG_FORMAT_RGBA;
-    printf("Width: %d, Height: %d\n", tileset.width, tileset.height);
-    uint32_t *tileset_pixels = (uint32_t *) std::malloc(sizeof(uint32_t) * tileset.width * tileset.height);
-
-    if (!png_image_finish_read(&tileset, nullptr, tileset_pixels, 0, nullptr)) {
-        fprintf(stderr, "libpng pooped itself: %s\n", tileset.message);
-        abort();
-    }
-
-    SDL_Surface* tileset_surface =
-        sdl(SDL_CreateRGBSurfaceFrom(tileset_pixels,
-                                     tileset.width,
-                                     tileset.height,
-                                     32,
-                                     tileset.width * 4,
-                                     0x000000FF,
-                                     0x0000FF00,
-                                     0x00FF0000,
-                                     0xFF000000));
-    SDL_Texture *tileset_texture =
-        sdl(SDL_CreateTextureFromSurface(renderer,
-                                         tileset_surface));
+    SDL_Texture *tileset_texture = load_texture_from_png_file(
+        renderer,
+        "fantasy_tiles.png");
 
     Tile_Texture wall_texture = {
         .srcrect = {120, 128, 16, 16},
         .texture = tileset_texture
     };
 
+    SDL_Texture *walking_texture = load_texture_from_png_file(
+        renderer,
+        "walking-12px-zoom.png");
+
+    constexpr int walking_frame_size = 48;
+    constexpr int walking_frame_count = 4;
+    constexpr int walking_frame_duration = 200;
+    int walking_frame_current = 0;
+    Tile_Texture walking_frames[walking_frame_count];
+
+    for (int i = 0; i < walking_frame_count; ++i) {
+        walking_frames[i].srcrect = {
+            .x = i * walking_frame_size,
+            .y = 0,
+            .w = walking_frame_size,
+            .h = walking_frame_size
+        };
+        walking_frames[i].texture = walking_texture;
+    }
+
+    Uint32 walking_frame_cooldown = walking_frame_duration;
+
+    int x = 0;
+
     bool quit = false;
+    const Uint8 *keyboard = SDL_GetKeyboardState(NULL);
     while (!quit) {
+        const Uint32 begin = SDL_GetTicks();
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -143,20 +181,31 @@ int main(int argc, char *argv[])
             }
         }
 
+        if (keyboard[SDL_SCANCODE_D]) {
+            x += 1;
+        } else if (keyboard[SDL_SCANCODE_A]) {
+            x -= 1;
+        }
+
         sdl(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
         sdl(SDL_RenderClear(renderer));
 
-        // sdl(SDL_RenderCopy(
-        //         renderer,
-        //         tileset_texture,
-        //         &tileset_surface->clip_rect,
-        //         &tileset_surface->clip_rect));
-
         render_level(renderer, wall_texture);
+        render_tile_texture(renderer, walking_frames[walking_frame_current],
+                            {x, 4 * TILE_SIZE - walking_frame_size,
+                             walking_frame_size, walking_frame_size});
 
         SDL_RenderPresent(renderer);
-    }
 
+        const Uint32 dt = SDL_GetTicks() - begin;
+
+        if (dt < walking_frame_cooldown) {
+            walking_frame_cooldown -= dt;
+        } else {
+            walking_frame_current = (walking_frame_current + 1) % walking_frame_count;
+            walking_frame_cooldown = walking_frame_duration;
+        }
+    }
     SDL_Quit();
 
     return 0;
