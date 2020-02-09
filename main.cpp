e510a192366b89d3f0b1fc71ab90f71e15a43f13
rexim <reximkut@gmail.com>
2020-02-10 00:47:38 +0700

    Introduce level rendering

--- main.cpp
+++ main.cpp
@@ -23,6 +23,46 @@ T *sdl(T *ptr)
     return ptr;
 }
 
+constexpr int TILE_SIZE = 64;
+
+enum class Tile
+{
+    Empty = 0,
+    Wall
+};
+
+constexpr int LEVEL_WIDTH = 5;
+constexpr int LEVEL_HEIGHT = 5;
+
+Tile level[LEVEL_HEIGHT][LEVEL_WIDTH] = {
+    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
+    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
+    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty},
+    {Tile::Wall,  Tile::Wall,  Tile::Wall,  Tile::Empty, Tile::Empty},
+    {Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty}
+};
+
+void render_level(SDL_Renderer *renderer)
+{
+    for (int y = 0; y < LEVEL_HEIGHT; ++y) {
+        for (int x = 0; x < LEVEL_WIDTH; ++x) {
+            switch (level[y][x]) {
+            case Tile::Empty: {
+            } break;
+
+            case Tile::Wall: {
+                sdl(SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255));
+                SDL_Rect rect = {
+                    x * TILE_SIZE, y * TILE_SIZE,
+                    TILE_SIZE, TILE_SIZE
+                };
+                sdl(SDL_RenderFillRect(renderer, &rect));
+            } break;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     sdl(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS));
@@ -51,6 +91,8 @@ int main(int argc, char *argv[])
 
         sdl(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
         sdl(SDL_RenderClear(renderer));
+
+        render_level(renderer);
 
         SDL_RenderPresent(renderer);
     }
