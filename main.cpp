b9c036ab6feccc8487a6387507ae236668b99aa6
rexim <reximkut@gmail.com>
2020-02-10 22:22:12 +0700

    TODO: baking assets into executable

--- main.cpp
+++ main.cpp
@@ -148,6 +148,7 @@ int main(void)
         tileset_texture
     };
 
+    // TODO: baking assets into executable
     SDL_Texture *walking_texture = load_texture_from_png_file(
         renderer,
         "walking-12px-zoom.png");
