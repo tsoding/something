3ded3a0f9b00af30d44da9844570f006a9abe42c
rexim <reximkut@gmail.com>
2020-02-25 05:22:45 +0700

    Compress vec.hpp

--- vec.hpp
+++ vec.hpp
@@ -7,97 +7,43 @@ struct Vec2
     T x, y;
 };
 
-template <typename T>
-Vec2<T> vec2(T x, T y)
-{
-    return {x, y};
-}
-
 using Vec2i = Vec2<int>;
 
-//////////////////////////////
-
-template <typename T>
-Vec2<T> operator+(Vec2<T> a, Vec2<T> b)
-{
-    return {a.x + b.x, a.y + b.y};
-}
-
-template <typename T>
-Vec2<T> operator-(Vec2<T> a, Vec2<T> b)
-{
-    return {a.x - b.x, a.y - b.y};
-}
-
-template <typename T>
-Vec2<T> operator*(Vec2<T> a, Vec2<T> b)
-{
-    return {a.x * b.x, a.y * b.y};
-}
-
-template <typename T>
-Vec2<T> operator/(Vec2<T> a, Vec2<T> b)
-{
-    return {a.x / b.x, a.y / b.y};
-}
-
-template <typename T>
-Vec2<T> &operator+=(Vec2<T> &a, Vec2<T> b)
-{
-    a = a + b;
-    return a;
-}
+template <typename T> Vec2<T> vec2(T x, T y) { return {x, y}; }
 
 //////////////////////////////
-
-template <typename T>
-Vec2<T> operator+(Vec2<T> a, T b)
-{
-    return {a.x + b, a.y + b};
-}
-
-template <typename T>
-Vec2<T> operator-(Vec2<T> a, T b)
-{
-    return {a.x - b, a.y - b};
-}
-
-template <typename T>
-Vec2<T> operator*(Vec2<T> a, T b)
-{
-    return {a.x * b, a.y * b};
-}
-
-template <typename T>
-Vec2<T> operator/(Vec2<T> a, T b)
-{
-    return {a.x / b, a.y / b};
-}
-
+// Vector x Vector
 //////////////////////////////
 
-template <typename T>
-Vec2<T> operator+(T a, Vec2<T> b)
-{
-    return {a + b.x, a + b.y};
-}
+template <typename T> Vec2<T> constexpr operator+(Vec2<T> a, Vec2<T> b) { return {a.x + b.x, a.y + b.y}; }
+template <typename T> Vec2<T> constexpr operator-(Vec2<T> a, Vec2<T> b) { return {a.x - b.x, a.y - b.y}; }
+template <typename T> Vec2<T> constexpr operator*(Vec2<T> a, Vec2<T> b) { return {a.x * b.x, a.y * b.y}; }
+template <typename T> Vec2<T> constexpr operator/(Vec2<T> a, Vec2<T> b) { return {a.x / b.x, a.y / b.y}; }
+template <typename T> Vec2<T> constexpr &operator+=(Vec2<T> &a, Vec2<T> b) { a = a + b; return a; }
+template <typename T> Vec2<T> constexpr &operator-=(Vec2<T> &a, Vec2<T> b) { a = a - b; return a; }
+template <typename T> Vec2<T> constexpr &operator*=(Vec2<T> &a, Vec2<T> b) { a = a * b; return a; }
+template <typename T> Vec2<T> constexpr &operator/=(Vec2<T> &a, Vec2<T> b) { a = a / b; return a; }
 
-template <typename T>
-Vec2<T> operator-(T a, Vec2<T> b)
-{
-    return {a - b.x, a - b.y};
-}
+//////////////////////////////
+// Vector x Scalar
+//////////////////////////////
 
-template <typename T>
-Vec2<T> operator*(T a, Vec2<T> b)
-{
-    return {a * b.x, a * b.y};
-}
+template <typename T> Vec2<T> constexpr operator+(Vec2<T> a, T b) { return {a.x + b, a.y + b}; } 
+template <typename T> Vec2<T> constexpr operator-(Vec2<T> a, T b) { return {a.x - b, a.y - b}; }
+template <typename T> Vec2<T> constexpr operator*(Vec2<T> a, T b) { return {a.x * b, a.y * b}; }
+template <typename T> Vec2<T> constexpr operator/(Vec2<T> a, T b) { return {a.x / b, a.y / b}; }
+template <typename T> Vec2<T> constexpr &operator+=(Vec2<T> &a, T b) { a = a + b; return a; }
+template <typename T> Vec2<T> constexpr &operator-=(Vec2<T> &a, T b) { a = a - b; return a; }
+template <typename T> Vec2<T> constexpr &operator*=(Vec2<T> &a, T b) { a = a * b; return a; }
+template <typename T> Vec2<T> constexpr &operator/=(Vec2<T> &a, T b) { a = a / b; return a; }
 
-template <typename T>
-Vec2<T> operator/(T a, Vec2<T> b)
-{
-    return {a / b.x, a / b.y};
-}
+//////////////////////////////
+// Scalar x Vector
+//////////////////////////////
+
+template <typename T> Vec2<T> constexpr operator+(T a, Vec2<T> b) { return {a + b.x, a + b.y}; }
+template <typename T> Vec2<T> constexpr operator-(T a, Vec2<T> b) { return {a - b.x, a - b.y}; }
+template <typename T> Vec2<T> constexpr operator*(T a, Vec2<T> b) { return {a * b.x, a * b.y}; }
+template <typename T> Vec2<T> constexpr operator/(T a, Vec2<T> b) { return {a / b.x, a / b.y}; }
 
 #endif  // VEC_HPP_
