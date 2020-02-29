4eca3bd2f442c8337d876c1e92ffdc2e5aea259b
rexim <reximkut@gmail.com>
2020-03-01 04:52:19 +0700

    Prefix filenames with `something_`

---
+++ src/something.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+#include <png.h>
+
+// READ THIS FIRST ---> https://en.wikipedia.org/wiki/Single_Compilation_Unit
+#include "something_error.cpp"
+#include "something_vec.cpp"
+#include "something_sprite.cpp"
+#include "something_level.cpp"
+#include "something_projectile.cpp"
+#include "something_entity.cpp"
+#include "something_main.cpp"
