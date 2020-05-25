d0739ba68d518bb0bf015bd5c2514eca5c24b0d3
rexim <reximkut@gmail.com>
2020-05-25 12:44:22 +0700

    Make Cached_Font::render_sv() work with empty Cached_Font

--- src/something_font.cpp
+++ src/something_font.cpp
@@ -15,29 +15,31 @@ void Cached_Font::populate_cache(SDL_Renderer *renderer)
 
 void Cached_Font::render_sv(SDL_Renderer *renderer, Vec2f position, SDL_Color color, String_View sv)
 {
-    int pen_x = 0;
-    for (size_t i = 0; i < sv.count; ++i) {
-        const Uint16 glyph = sv.data[i];
-        assert(glyph < CACHED_FONT_CAPACITY);
+    if (font) {
+        int pen_x = 0;
+        for (size_t i = 0; i < sv.count; ++i) {
+            const Uint16 glyph = sv.data[i];
+            if (0 < glyph && glyph < CACHED_FONT_CAPACITY && glyph_cache[glyph]) {
+                int minx, maxx, miny, maxy, advance;
+                stec(TTF_GlyphMetrics(font, glyph, &minx, &maxx, &miny, &maxy, &advance));
+                const int w = maxx - minx;
+                const int h = maxy - miny;
+                const SDL_Rect srcrect = {
+                    minx, TTF_FontAscent(font) - maxy,
+                    w, h
+                };
+                const SDL_Rect dstrect = {
+                    (int) floorf(position.x) + pen_x + minx,
+                    (int) floorf(position.y) + miny + TTF_FontAscent(font) - maxy,
+                    w, h
+                };
 
-        int minx, maxx, miny, maxy, advance;
-        stec(TTF_GlyphMetrics(font, glyph, &minx, &maxx, &miny, &maxy, &advance));
-        const int w = maxx - minx;
-        const int h = maxy - miny;
-        const SDL_Rect srcrect = {
-            minx, TTF_FontAscent(font) - maxy,
-            w, h
-        };
-        const SDL_Rect dstrect = {
-            (int) floorf(position.x) + pen_x + minx,
-            (int) floorf(position.y) + miny + TTF_FontAscent(font) - maxy,
-            w, h
-        };
-
-        sec(SDL_SetTextureColorMod(glyph_cache[glyph], color.r, color.g, color.b));
-        sec(SDL_SetTextureAlphaMod(glyph_cache[glyph], color.a));
-        sec(SDL_RenderCopy(renderer, glyph_cache[glyph], &srcrect, &dstrect));
-        pen_x += advance;
+                sec(SDL_SetTextureColorMod(glyph_cache[glyph], color.r, color.g, color.b));
+                sec(SDL_SetTextureAlphaMod(glyph_cache[glyph], color.a));
+                sec(SDL_RenderCopy(renderer, glyph_cache[glyph], &srcrect, &dstrect));
+                pen_x += advance;
+            }
+        }
     }
 }
 
