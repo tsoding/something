6353232e907a82bc03439e0b9c45a5cee9931770
rexim <reximkut@gmail.com>
2020-08-30 20:39:03 +0700

    Add TODO(#178)

--- src/something_game.hpp
+++ src/something_game.hpp
@@ -82,7 +82,7 @@ struct Game
     Sample_Mixer mixer;
     const Uint8 *keyboard;
     Popup popup;
-    // TODO: disable game console in release mode
+    // TODO(#178): disable game console in release mode
     Console console;
 
     Frame_Animat entity_walking_animat;
