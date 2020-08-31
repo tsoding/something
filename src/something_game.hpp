15b61ac72f144c72255ef1f1b9bdd42cc30c5132
rexim <reximkut@gmail.com>
2020-08-31 18:47:12 +0700

    Remove TODO(#136)

--- src/something_game.hpp
+++ src/something_game.hpp
@@ -65,7 +65,6 @@ const size_t ITEMS_COUNT = 69;
 const size_t CAMERA_LOCKS_CAPACITY = 200;
 const size_t ROOM_ROW_COUNT = 8;
 
-// TODO(#136): camera anchors in the centers of the "rooms"
 struct Game
 {
     bool quit;
