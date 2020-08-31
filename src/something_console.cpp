67e58480ec22db22c93f6f997a9dce1c03a77fdc
rexim <reximkut@gmail.com>
2020-08-31 18:47:11 +0700

    Remove TODO(#159)

--- src/something_console.cpp
+++ src/something_console.cpp
@@ -208,7 +208,6 @@ void Console::handle_event(SDL_Event *event, Game *game)
     // TODO(#158): Backtick event bleeds into the Console
     if (enabled) {
         // TODO(#146): No support for delete or backspace in console
-        // TODO(#159): Console does not integrate with the OS clipboard
         if (popup_enabled) {
             switch (event->type) {
             case SDL_KEYDOWN: {
