8e953788d59e0965105ac57f1d00dbad27156ee8
rexim <reximkut@gmail.com>
2020-08-31 21:27:18 +0700

    Introduce help command

--- src/something_commands.hpp
+++ src/something_commands.hpp
@@ -3,6 +3,7 @@
 
 struct Game;
 
+void command_help(Game *game, String_View args);
 void command_quit(Game *game, String_View args);
 void command_reset(Game *game, String_View args);
 void command_spawn_enemy(Game *game, String_View args);
@@ -14,16 +15,18 @@ void command_set(Game *game, String_View args);
 struct Command
 {
     String_View name;
+    String_View description;
     void (*run)(Game *game, String_View args);
 };
 
 const Command commands[] = {
-    {"quit"_sv, command_quit},
-    {"reset"_sv, command_reset},
-    {"spawn_enemy"_sv, command_spawn_enemy},
-    {"close"_sv, command_close},
+    {"help"_sv,        "Print this help"_sv,                 command_help},
+    {"quit"_sv,        "Quit the game"_sv,                   command_quit},
+    {"reset"_sv,       "Reset the state of the entities"_sv, command_reset},
+    {"spawn_enemy"_sv, "Spawn an enemy"_sv,                  command_spawn_enemy},
+    {"close"_sv,       "Close the console"_sv,               command_close},
 #ifndef SOMETHING_RELEASE
-    {"set"_sv, command_set},
+    {"set"_sv,         "Set the value of the variable"_sv,   command_set},
 #endif // SOMETHING_RELEASE
 };
 const size_t commands_count = sizeof(commands) / sizeof(commands[0]);
