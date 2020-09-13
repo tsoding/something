5124f210ac45ba28a33d1f10c4819b1e0cd87212
rexim <reximkut@gmail.com>
2020-09-13 20:05:24 +0700

    Add TODO(#199)

--- src/something_console.hpp
+++ src/something_console.hpp
@@ -31,7 +31,7 @@ struct Console
     {
         char entries[CONSOLE_HISTORY_CAPACITY][CONSOLE_COLUMNS];
         size_t entry_sizes[CONSOLE_HISTORY_CAPACITY];
-        // TODO: try to organize history as end+count instead of begin+count
+        // TODO(#199): try to organize history as end+count instead of begin+count
         int begin;
         int count;
         int cursor;
