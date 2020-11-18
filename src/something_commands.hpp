73e6c866b6b7518dda8ff0cdc4ea1dfd815f521e
Danil Kolumbet <kolumbetko@gmail.com>
2020-11-19 01:55:30 +0200

    Sort console commands alphabetically

--- src/something_commands.hpp
+++ src/something_commands.hpp
@@ -25,18 +25,20 @@ struct Command
 };
 
 const Command commands[] = {
-    {"help"_sv,        "Print this help"_sv,                  command_help},
-    {"quit"_sv,        "Quit the game"_sv,                    command_quit},
-    {"reset"_sv,       "Reset the state of the entities"_sv,  command_reset},
-    {"spawn_enemy"_sv, "Spawn an enemy"_sv,                   command_spawn_enemy},
     {"close"_sv,       "Close the console"_sv,                command_close},
-#ifndef SOMETHING_RELEASE
-    {"set"_sv,         "Set the value of a variable"_sv,      command_set},
-    {"reload"_sv,      "Reloads the configuration file"_sv,   command_reload},
-#endif // SOMETHING_RELEASE
-    {"save_room"_sv,   "Save current room as new file"_sv,    command_save_room},
+    {"help"_sv,        "Print this help"_sv,                  command_help},
     {"history"_sv,     "Print the history of the Console"_sv, command_history},
     {"noclip"_sv,      "Turn on/off noclip mode"_sv,          command_noclip},
+    {"quit"_sv,        "Quit the game"_sv,                    command_quit},
+#ifndef SOMETHING_RELEASE
+    {"reload"_sv,      "Reloads the configuration file"_sv,   command_reload},
+#endif // SOMETHING_RELEASE
+    {"reset"_sv,       "Reset the state of the entities"_sv,  command_reset},
+    {"save_room"_sv,   "Save current room as new file"_sv,    command_save_room},
+#ifndef SOMETHING_RELEASE
+    {"set"_sv,         "Set the value of a variable"_sv,      command_set},
+#endif // SOMETHING_RELEASE
+    {"spawn_enemy"_sv, "Spawn an enemy"_sv,                   command_spawn_enemy},
 };
 const size_t commands_count = sizeof(commands) / sizeof(commands[0]);
 
