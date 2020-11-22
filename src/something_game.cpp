2ed2e61a050b7295bb715115610a220a608038c7
rexim <reximkut@gmail.com>
2020-11-22 19:59:26 +0700

    (#287) Make water projectile kill fire projectile

--- src/something_game.cpp
+++ src/something_game.cpp
@@ -920,7 +920,8 @@ void Game::projectile_collision(Projectile *a, Projectile *b)
     {
         if (rects_overlap(a->hitbox(), b->hitbox())) {
             if ((a->kind == Projectile_Kind::Ice && b->kind == Projectile_Kind::Fire) ||
-                (a->kind == Projectile_Kind::Rock && b->kind == Projectile_Kind::Water))
+                (a->kind == Projectile_Kind::Rock && b->kind == Projectile_Kind::Water) ||
+                (a->kind == Projectile_Kind::Fire && b->kind == Projectile_Kind::Water))
             {
                 swap(&a, &b);
             }
@@ -937,6 +938,10 @@ void Game::projectile_collision(Projectile *a, Projectile *b)
 
             if (a->kind == Projectile_Kind::Water && b->kind == Projectile_Kind::Rock) {
                 a->kill();
+                b->kill();
+            }
+
+            if (a->kind == Projectile_Kind::Water && b->kind == Projectile_Kind::Fire) {
                 b->kill();
             }
         }
