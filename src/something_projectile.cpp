b0d90f2ac6a234993f995706f150409b9c12e35b
rexim <reximkut@gmail.com>
2020-11-30 20:55:57 +0700

    Remove TODO(#285)

--- src/something_projectile.cpp
+++ src/something_projectile.cpp
@@ -145,7 +145,6 @@ Projectile fire_projectile(Vec2f pos, Vec2f vel, Entity_Index shooter)
 Projectile rock_projectile(Vec2f pos, Vec2f vel, Entity_Index shooter)
 {
     Projectile result = {};
-    // TODO(#285): there is nothing rock projectiles can damage for now
     result.kind          = Projectile_Kind::Rock;
     result.tile_damage   = Tile_Damage::None;
     result.state         = Projectile_State::Active;
