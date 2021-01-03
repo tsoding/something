fdb899a9564efa1398e32a70820acefaecb7b9a3
rexim <reximkut@gmail.com>
2021-01-03 20:21:02 +0700

    (#342) Improve Spike Wave variation

--- src/something_spike.hpp
+++ src/something_spike.hpp
@@ -13,7 +13,7 @@ struct Spike
     Spike_State state;
     Vec2f pos;
     float scale;
-    Index<Texture> texture_index;
+    Index<Sprite> sprite_index;
     float a;                     // 0.0..1.0
 
     void render(SDL_Renderer *renderer, Camera camera);
@@ -30,6 +30,7 @@ struct Spike_Wave
     Vec2f dir;
     int count;
     float cooldown;
+    float scale;
 
     void update(float dt, Game *game);
     void activate(Vec2f pos, Vec2f dir);
