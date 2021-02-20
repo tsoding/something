4c347ee1646c42bbe79128bb772abfec78ff5bac
rexim <reximkut@gmail.com>
2021-02-20 20:53:23 +0700

    Add config

--- src2/something.cpp
+++ src2/something.cpp
@@ -5,6 +5,7 @@
 using namespace aids;
 
 #include "./something_sdl.cpp"
+#include "./something_config.cpp"
 #include "./something_game.cpp"
 #include "./something_player.cpp"
 #include "./something_main.cpp"
---
+++ src2/something_config.cpp
@@ -0,0 +1,34 @@
+#include "something_config.hpp"
+
+Config config = {};
+
+
+void Config::load_file(const char *file_path)
+{
+    auto content =
+        unwrap_or_panic(
+            read_file_as_string_view(
+                file_path,
+                &config_file_buffer),
+            "Could not read file: ", file_path);
+
+    for (size_t line_number = 1; content.count > 0; ++line_number) {
+        auto line = content.chop_by_delim('\n');
+        auto var_def = line.chop_by_delim('#').trim();
+
+        if (var_def.count > 0) {
+            auto name = var_def.chop_by_delim(':').trim();
+            auto type = var_def.chop_by_delim('=').trim();
+            auto value = var_def.trim();
+            if (type == "float"_sv) {
+                todo("Parsing float is not implemented");
+            } else if (type == "int"_sv) {
+                todo("Parsing int is not implemented");
+            } else if (type == "color"_sv) {
+                todo("Parsing color is not implemented");
+            } else {
+                panic(file_path, ":", line_number, ": Unknown type `", type, "`");
+            }
+        }
+    }
+}
