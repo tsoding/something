b5b9740c5e166e9639f66faa5d6b9d0cf5583f1e
rexim <reximkut@gmail.com>
2021-02-27 20:27:19 +0700

    Introduce Texture struct
    We are not using it yet, but we are planning to Kappa

--- src2/something.cpp
+++ src2/something.cpp
@@ -9,7 +9,11 @@ using namespace aids;
 typedef float Seconds;
 typedef Uint32 Milliseconds;
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "./stb_image.h"
+
 #include "./something_renderer.cpp"
+#include "./something_texture.cpp"
 #include "./something_config.cpp"
 #include "./something_game.cpp"
 #include "./something_player.cpp"
